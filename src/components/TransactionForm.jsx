4d779b56cbb211a4fee2bb0433cc56bd86069352
cahoma0594 <contacto.andresgomez@gmail.com>
2026-05-03 00:18:59 +0200

    Fix function path

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -47,7 +47,7 @@ export default function TransactionForm({ initial, categories, familyId, userId,
       })
 
       
-      const response = await fetch('/api/ocr', {
+      const response = await fetch('/.netlify/functions/ocr', {
         method: 'POST',
     	headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
